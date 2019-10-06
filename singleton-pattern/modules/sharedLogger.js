d6454bb48346feb3a69dc09324c27a3c8d4f8c15
brainyfarm <brainyfarm@gmail.com>
2019-10-06 09:09:12 +0100

    comment shareLogger code

--- singleton-pattern/modules/sharedLogger.js
+++ singleton-pattern/modules/sharedLogger.js
@@ -1,7 +1,11 @@
 const { log } = require('console');
 
+/**
+ * Shared logger module which will be used everywhere
+ */
 class FancyLogger {
   constructor() {
+    // Check if an instance exists otherwise, set it to this instance
     if (!FancyLogger.instance) {
       this.logs = [];
       FancyLogger.instance = this;
@@ -18,7 +22,9 @@ class FancyLogger {
   }
 }
 
+// Create and export the FancyLoger instance
 const logger = new FancyLogger();
+//Prevent further changes to the single instance
 Object.freeze(logger);
 
 module.exports = logger;
